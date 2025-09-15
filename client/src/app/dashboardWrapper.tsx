0a6efdea2148721f8f91f6c3a1eedf26b6ff434b
Johno <johno.fisher01@gmail.com>
2025-09-15 19:14:26 +0100

    Navbar + Settings added

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,16 +1,15 @@
-
+import React from 'react'
+import Navbar from "@/app/(components)/Navbar"
 
 const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
   return <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
     { /* Sidebar */ }
     Sidebar
-    <div className="bg-blue-500 text-white p-4">
-  Tailwind test: This should be a blue box!
-</div>
+
 
     <main className={`dark:bg-dark-bg  flex w-full flex-col bg-gray-50 md:pl-64`}>
         {/* Navbar */}
-        navbar
+        <Navbar />
         {children}
 
         </main>
