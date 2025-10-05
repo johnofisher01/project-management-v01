f8c9a50f86d9f49109df961b886c0f080f1e45a7
Johno <johnofisherdrummer@gmail.com>
2025-10-05 10:05:01 +0100

    dark mode

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -20,7 +20,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      <Sidebar isSidebarCollapsed={isSidebarCollapsed} />
+      <Sidebar />
 
       <main
         className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${
